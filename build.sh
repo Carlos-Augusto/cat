84143d979010671db7332de92f47462b1612fddd
Carlos Sanchez <caaugustoss@gmail.com>
2020-09-14 18:41:06 +0200

    Improving install script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 INSTALL_FOLDER=$1
+VERSION=0.0.1
+JARNAME="catalina-$VERSION.jar"
 
 if [ -z "$1" ]
   then
@@ -9,17 +11,20 @@ if [ -z "$1" ]
     exit 1
 fi
 
-echo "Building/Installing in $INSTALL_FOLDER ..."
+echo "Building/Installing in $INSTALL_FOLDER ... version $VERSION"
 rm "$INSTALL_FOLDER" -rf
 mvn clean package
 mkdir "$INSTALL_FOLDER"
 
-cp target/catalina-1.0.0.jar "$INSTALL_FOLDER"
+cp "target/$JARNAME" "$INSTALL_FOLDER"
 cp target/lib "$INSTALL_FOLDER" -r
 touch "$INSTALL_FOLDER"/catalina.sh
 echo '#!/usr/bin/env bash' >> "$INSTALL_FOLDER"/catalina.sh
 echo 'set -e' >> "$INSTALL_FOLDER"/catalina.sh
 echo "cd $INSTALL_FOLDER" >> "$INSTALL_FOLDER"/catalina.sh
-echo 'java -cp catalina-1.0.0.jar com.flatmappable.Catalina "$@"' >> "$INSTALL_FOLDER"/catalina.sh
+echo "java -cp $JARNAME com.flatmappable.Catalina \"$*\"" >> "$INSTALL_FOLDER"/catalina.sh
 chmod +x "$INSTALL_FOLDER"/catalina.sh
+
+echo "For quick access, add these like to your profile of bashrc file"
+echo "export PATH=$INSTALL_FOLDER:\$PATH"
 
