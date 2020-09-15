f46758c49360c333503d1d49c8b31e3c5b10e524
Carlos Sanchez <caaugustoss@gmail.com>
2020-09-15 09:43:37 +0200

    Changing version in build script // Minor formatting

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 INSTALL_FOLDER=$1
-VERSION=0.0.2
+VERSION=0.0.3
 JARNAME="catalina-$VERSION.jar"
 
 if [ -z "$1" ]
--- src/main/scala/com/flatmappable/CatalinaHttp.scala
+++ src/main/scala/com/flatmappable/CatalinaHttp.scala
@@ -21,16 +21,17 @@ object CatalinaHttp extends cask.MainRoutes with LazyLogging {
     try {
       val identity = Try(UUID.fromString(uuid)).getOrElse(throw BadRequestException("Invalid uuid"))
       val body = request.bytes
-      if(body.isEmpty) throw BadRequestException("Empty body")
+      if (body.isEmpty) throw BadRequestException("Empty body")
       val privateKey = request.headers.get("x-pk").flatMap(_.headOption).getOrElse(throw BadRequestException("No x-pk"))
       val pass = request.headers.get("x-pass").flatMap(_.headOption).getOrElse(throw BadRequestException("No x-pass"))
 
       val (_, upp, hash) = DataGenerator.single(identity, body, privateKey, Protocol.Format.MSGPACK)
       val res = DataSending.send(identity, pass, DataGenerator.toBase64(hash), DataGenerator.toHex(upp))
 
-      if(res.status >= 200 && res.status < 300){
+      if (res.status >= 200 && res.status < 300) {
         cask.Response(DataGenerator.toBase64(hash), res.status)
       } else {
+        logger.error(s"Error Sending UPP=${DataGenerator.toBase64(hash)} Status=${res.status}")
         cask.Response(s"Error Sending UPP with Hash=${DataGenerator.toBase64(hash)}", res.status)
       }
 
@@ -38,7 +39,7 @@ object CatalinaHttp extends cask.MainRoutes with LazyLogging {
       case BadRequestException(message) =>
         logger.error("Bad Request={}", message)
         cask.Response(message, 400)
-      case e =>
+      case e: Exception =>
         logger.error("Internal error", e)
         cask.Response("Internal Error", 500)
     }
