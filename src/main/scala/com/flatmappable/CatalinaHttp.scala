d51d5a049333b3f38b8cfb98baf0b3fb217a0f26
Carlos Sanchez <caaugustoss@gmail.com>
2020-09-15 07:56:40 +0200

    Improving send endpoint

--- src/main/scala/com/flatmappable/CatalinaHttp.scala
+++ src/main/scala/com/flatmappable/CatalinaHttp.scala
@@ -2,9 +2,14 @@ package com.flatmappable
 
 import java.util.UUID
 
+import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.protocol.Protocol
 
-object CatalinaHttp extends cask.MainRoutes {
+import scala.util.Try
+
+object CatalinaHttp extends cask.MainRoutes with LazyLogging {
+
+  case class BadRequestException(message: String) extends Exception(message)
 
   @cask.get("/")
   def hello() = {
@@ -13,14 +18,30 @@ object CatalinaHttp extends cask.MainRoutes {
 
   @cask.post("/send/:uuid")
   def send(uuid: String, request: cask.Request) = {
-    val identity = UUID.fromString(uuid)
-    val body = request.bytes
-    val privateKey = request.headers("x-pk").headOption.getOrElse(throw new Exception("No x-pk"))
-    val pass = request.headers("x-pass").headOption.getOrElse(throw new Exception("No x-pass"))
-    val (_, upp, hash) = DataGenerator.single(identity, body, privateKey, Protocol.Format.MSGPACK)
-    val res = DataSending.send(identity, pass, DataGenerator.toBase64(hash), DataGenerator.toHex(upp))
+    try {
+      val identity = Try(UUID.fromString(uuid)).getOrElse(throw BadRequestException("Invalid uuid"))
+      val body = request.bytes
+      if(body.isEmpty) throw BadRequestException("Empty body")
+      val privateKey = request.headers.get("x-pk").flatMap(_.headOption).getOrElse(throw BadRequestException("No x-pk"))
+      val pass = request.headers.get("x-pass").flatMap(_.headOption).getOrElse(throw BadRequestException("No x-pass"))
 
-    cask.Response(DataGenerator.toBase64(hash), res.status)
+      val (_, upp, hash) = DataGenerator.single(identity, body, privateKey, Protocol.Format.MSGPACK)
+      val res = DataSending.send(identity, pass, DataGenerator.toBase64(hash), DataGenerator.toHex(upp))
+
+      if(res.status >= 200 && res.status < 300){
+        cask.Response(DataGenerator.toBase64(hash), res.status)
+      } else {
+        cask.Response(s"Error Sending UPP with Hash=${DataGenerator.toBase64(hash)}", res.status)
+      }
+
+    } catch {
+      case BadRequestException(message) =>
+        logger.error("Bad Request={}", message)
+        cask.Response(message, 400)
+      case e =>
+        logger.error("Internal error", e)
+        cask.Response("Internal Error", 500)
+    }
   }
 
   initialize()
