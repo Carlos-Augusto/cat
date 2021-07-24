e6b569eab8b1f2d0c4e216dd20f4ea4021f2a5b7
CarlosAugusto <caaugustoss@gmail.com>
2021-07-24 23:26:58 +0200

    Making host configurable

--- src/main/scala/com/flatmappable/util/Configs.scala
+++ src/main/scala/com/flatmappable/util/Configs.scala
@@ -7,6 +7,7 @@ object Configs {
   final val ENV: Symbol = Symbol(conf.getString("environment"))
   final val DATA_FOLDER: String = conf.getString("dataFolder")
   final val CAT_HTTP_PORT: Int = conf.getInt("catHttpPort")
+  final val CAT_HTTP_HOST: String = conf.getString("catHttpHost")
   final val DATA_SENDING_URL: String = conf.getString("dataSendingUrl")
   final val KEY_REGISTRATION_URL: String = conf.getString("keyRegistrationUrl")
   final val SIMPLE_VERIFICATION_URL: String = conf.getString("simpleVerificationUrl")
